78d3b1ece038d0d7c465eb94c2c5500c609b4b8b
Callupe72 <paulleduc72@gmail.com>
2021-09-07 15:56:05 +0200

    Starting pause menu

--- Assets/Scripts/FPSCamera.cs
+++ Assets/Scripts/FPSCamera.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSCamera : MonoBehaviour
 {
 
     [Header("Cam")]
     public float mouseSensitivity = 100f;
+    public float xMouseSensivity { get; set; } = 50f;
+    public float yMouseSensivity { get; set; } = 50f;
 
     public Transform playerBody;
 
     float xRotation = 0f;
-
 
     [Header("Raycast")]
     public Transform head;
     public float rangePlayer;
     public LayerMask layerInteractable;
 
+    [Header("Sensivity Sliders")]
+    public Slider xSlider;
+    public Slider ySlider;
+
+    [Header("Sensivity numbers")]
+    public TextMeshProUGUI xSliderText;
+    public TextMeshProUGUI ySliderText;
+
     void Start()
     {
+        LoadSensivity();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -29,10 +39,39 @@ public class FPSCamera : MonoBehaviour
         Raycast();
     }
 
+    void LoadSensivity()
+    {
+        if (PlayerPrefs.GetFloat("xMouseSensivity") == 0)
+        {
+            xMouseSensivity = 50f;
+            yMouseSensivity = 50f;
+            SaveSensivity();
+        }
+        else
+        {
+            xMouseSensivity = PlayerPrefs.GetFloat("xMouseSensivity");
+            yMouseSensivity = PlayerPrefs.GetFloat("yMouseSensivity");
+        }
+
+        xSlider.value = xMouseSensivity;
+        ySlider.value = yMouseSensivity;
+    }
+
+    public void SaveSensivity()
+    {
+        PlayerPrefs.SetFloat("xMouseSensivity", xSlider.value);
+        PlayerPrefs.SetFloat("yMouseSensivity", ySlider.value);
+    }
+
+    public void ActualiseSensivityText()
+    {
+
+    }
+
     void CameraFollowMouse()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * xMouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * yMouseSensivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
@@ -44,11 +83,12 @@ public class FPSCamera : MonoBehaviour
     void Raycast()
     {
         RaycastHit hit;
-        Debug.DrawRay(head.position, transform.forward * rangePlayer);
-
-        if (Physics.Raycast(head.position, transform.forward, out hit, rangePlayer, layerInteractable))
-        {
-
+        Debug.DrawRay(transform.position, transform.forward * rangePlayer);
+        if (Input.GetMouseButton(0))
+        { 
+            if (Physics.Raycast(transform.position, transform.forward, out hit, rangePlayer, layerInteractable))
+            {
+            }
         }
     }
 
