b551cec0970ed58c5105c58aabc195a7514d0af5
Antirion779 <73186199+Antirion779@users.noreply.github.com>
2021-09-07 18:54:04 +0200

    début changement de dir + prio prise

--- Assets/Scripts/Baby/BabyMovement.cs
+++ Assets/Scripts/Baby/BabyMovement.cs
@@ -12,23 +12,42 @@ public class BabyMovement : MonoBehaviour
     [SerializeField] float speed;
     private Vector3 coinPosition;
 
+    [Header("Condition")]
+    [SerializeField] bool wantToDie = false;
+    GameObject nearestCoin = null;
+    [SerializeField] bool hasToChangeDir;
+
     private void Start()
     {
-        wichCoin = Random.Range(0, coin.Count);
-        coinPosition = coin[wichCoin].transform.position - transform.position;
+        coinPosition = Vector3.forward;
 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        coinPosition = coin[wichCoin].transform.position - transform.position; //Peut être en faire une liste pour qu'ils foncent sur un aléatoire
-        coinPosition.Normalize();
-
-        transform.LookAt(coin[wichCoin].transform);
-
-
+    {  
         rb.velocity = coinPosition * speed;
+
+        //Si le bebe change de dir et veut die
+        if (hasToChangeDir && wantToDie)
+        {
+            float coinTested;
+            for (int i = 0; i < coin.Count; i++)//pour chaques coin test si il est plus proche que les autres
+            {
+                coinTested = Vector3.Distance(coin[i].transform.position, transform.position);
+                nearestCoin = coin[wichCoin];
+                if (coinTested < Vector3.Distance(nearestCoin.transform.position, transform.position))
+                {
+                    nearestCoin = coin[i];
+                }
+
+                hasToChangeDir = false;
+            }
+            rb.velocity = Vector3.zero;
+            coinPosition = new Vector3 (nearestCoin.transform.position.x, nearestCoin.transform.position.y, nearestCoin.transform.position.z);
+            transform.LookAt(coin[wichCoin].transform);
+        }
+
 
         RaycastHit hit;
         Debug.DrawRay(transform.position, Vector3.down * 100);
