7e3b155f77f7f3d3b9e75d89e8b8a9698dd7b0c9
Callupe72 <paulleduc72@gmail.com>
2021-09-08 11:10:42 +0200

    AddCoin

---
+++ Assets/Corners.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corners : MonoBehaviour
+{
+    public float cornerRadius = 2f;
+    public float timeBeforeKill = 1f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, cornerRadius);
+    }
+
+    void Update()
+    {
+        foreach (Collider item in Physics.OverlapSphere(transform.position, cornerRadius))
+        {
+            if (item.gameObject.CompareTag("Baby"))
+            {
+                Destroy(item.gameObject, timeBeforeKill);
+                FindObjectOfType<Spawn>().SpawnABaby();
+                //Score
+            }
+        }
+    }
+}
